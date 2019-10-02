0e083e91414dc0422993300f2556a5cd6a5d8066
Demara Ramadhani <demararamadhanii@gmail.com>
2019-10-02 15:26:40 +0700

    remove .git inside .git

---
+++ TugasModul1/src/tugas1modul1/Tugas1modul1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas1modul1;
+
+/**
+ *
+ * @author ARA
+ */
+public class Tugas1modul1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
